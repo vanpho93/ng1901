752cb92d138808bebf76e2ba23251e28cb1a1caf
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-20 19:00:05 +0700

    list word ts

--- src/app/components/condition.component.ts
+++ src/app/components/condition.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
     selector: 'app-condition',
     template: `
         <div>
-            <img [src]="imageSrc" />
+            <img [src]="word.isMemorized ? imageSrc1 : imageSrc2" />
             <h3>{{word.en}}</h3>
             <p>{{word.vn}}</p>
             <button
@@ -37,7 +37,8 @@ export class ConditionComponent {
         vn: 'Mot',
         isMemorized: false
     };
-    imageSrc = 'https://zmp3-photo.zadn.vn/banner/c/b/d/3/cbd38a122a6a6897fd906d68a97d31b1.jpg';
+    imageSrc1 = 'https://zmp3-photo.zadn.vn/banner/c/b/d/3/cbd38a122a6a6897fd906d68a97d31b1.jpg';
+    imageSrc2 = 'https://zmp3-photo.zadn.vn/banner/c/0/5/a/c05a9a464d4cec9814f8bfa1cd6d729b.jpg';
     toggle() {
         this.word.isMemorized = !this.word.isMemorized;
     }
--- src/app/components/list-word/list-word.component.ts
+++ src/app/components/list-word/list-word.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-list-word',
   templateUrl: './list-word.component.html',
   styleUrls: ['./list-word.component.css']
 })
-export class ListWordComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ListWordComponent {
+  words = [
+    { _id: 'a', en: 'One', vn: 'Mot' },
+    { _id: 'b', en: 'Two', vn: 'Hai' },
+    { _id: 'c', en: 'Three', vn: 'Ba' },
+    { _id: 'd', en: 'Four', vn: 'Bon' },
+  ];
 }
