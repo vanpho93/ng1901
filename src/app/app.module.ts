162e437654f4c9ee983d01f1cca9ce21be685a1d
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-20 18:30:35 +0700

    condition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { WordComponent } from './components/word.component';
+import { ConditionComponent } from './components/condition.component';
 import { ListWordComponent } from './components/list-word/list-word.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WordComponent,
+    ConditionComponent,
     ListWordComponent,
   ],
   imports: [
---
+++ src/app/components/condition.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-condition',
+    template: `
+        <div>
+            <p>Condition component</p>
+        </div>
+    `
+})
+
+export class ConditionComponent {
+}
