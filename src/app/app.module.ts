abcfa334ce48b34e86d96dc2166b0d084e160f1f
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-20 18:26:58 +0700

    click

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { WordComponent } from './components/word.component';
+import { ListWordComponent } from './components/list-word/list-word.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WordComponent
+    WordComponent,
+    ListWordComponent,
   ],
   imports: [
     BrowserModule
--- src/app/components/word.component.ts
+++ src/app/components/word.component.ts
@@ -3,11 +3,36 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-word',
     template: `
-        <p>Word Component</p>
+        <div>
+            <p>Count = {{ count }}</p>
+            <h3>Count = {{ count }}</h3>
+            <h1>Count = {{ count }}</h1>
+            <div>Count = {{ count }}</div>
+            <button
+                class="btn btn-success"
+                (click)="count = count + 1"
+            >
+                Increase
+            </button>
+            <button
+                class="btn btn-danger"
+                (click)="count = count - 1"
+            >
+                Descrease
+            </button>
+            <button
+                class="btn btn-warning"
+                (click)="count = 1"
+            >
+                Reset
+            </button>
+        </div>
     `,
     styles: [
         `p { color: red; }`
     ]
 })
 
-export class WordComponent {}
+export class WordComponent {
+    count = 1;
+}
