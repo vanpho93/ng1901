bd19a33b51cb57b544d87cbbc3d0e23aa47e1b50
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-20 19:05:15 +0700

    xong

--- src/app/components/list-word/list-word.component.ts
+++ src/app/components/list-word/list-word.component.ts
@@ -8,9 +8,14 @@ import { Component } from '@angular/core';
 
 export class ListWordComponent {
   words = [
-    { _id: 'a', en: 'One', vn: 'Mot' },
-    { _id: 'b', en: 'Two', vn: 'Hai' },
-    { _id: 'c', en: 'Three', vn: 'Ba' },
-    { _id: 'd', en: 'Four', vn: 'Bon' },
+    { _id: 'a', en: 'One', vn: 'Mot', isMemorized: true },
+    { _id: 'b', en: 'Two', vn: 'Hai', isMemorized: false },
+    { _id: 'c', en: 'Three', vn: 'Ba', isMemorized: true },
+    { _id: 'd', en: 'Four', vn: 'Bon', isMemorized: false },
   ];
+
+  toggle(_id: string) {
+    const word = this.words.find(w => w._id === _id);
+    word.isMemorized = !word.isMemorized;
+  }
 }
