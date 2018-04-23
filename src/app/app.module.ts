89466382b9475e178effe3586695d9230ca90005
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-23 18:05:58 +0700

    handle input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { WordComponent } from './components/word.component';
@@ -16,7 +17,8 @@ import { ListWordComponent } from './components/list-word/list-word.component';
     MyInputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/my-input.component.ts
+++ src/app/components/my-input.component.ts
@@ -4,14 +4,32 @@ import { Component } from '@angular/core';
     selector: 'app-my-input',
     template: `
         <div class="form-group" style="width: 300px; padding-top: 20px">
-            <input class="form-control" placeholder="Email">
+            <input
+                class="form-control"
+                placeholder="Email"
+                [(ngModel)]="email"
+            >
             <br>
-            <input class="form-control" placeholder="Password" type="password">
+            <input
+                class="form-control"
+                placeholder="Password"
+                type="password"
+                [(ngModel)]="password"
+            >
             <br>
-            <button class="btn btn-success form-control">Sign In</button>
+            <button class="btn btn-success form-control" (click)="email = ''">
+                Sign In
+            </button>
+            <pre>Email: {{ email }}</pre>
+            <pre>Password: {{ password }}</pre>
         </div>
     `
 })
 
 export class MyInputComponent {
+    email = '';
+    password = '';
+    show(x) {
+        console.log(x.value);
+    }
 }
